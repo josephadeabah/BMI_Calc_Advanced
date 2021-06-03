65a794b42ed47aaa509f7251f017389c2a199401
josephadeabah <joseph.adeabah@amalitech.org>
2021-06-03 15:14:20 +0000

    Modified my files and added styling

--- jscript.js
+++ jscript.js
@@ -29,3 +29,31 @@
 /*document.write(calculateBMI(1534,6))*/
 
 
+let first = document.getElementById("btn");
+first.addEventListener('click', () => {
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+    let results = document.getElementById("results");
+    message = "";
+    let BMI = Math.round(weight / Math.pow(height, 2));
+
+    if (BMI < 18.5)
+    {
+       message = "You are Underweight"
+    }
+    else if (BMI >= 18.5 && BMI < 24.9)
+    {
+      message = "You have a Normal Weight"
+    }
+    else if (BMI >= 24.9)
+    {
+      message = "You Are Over Weight" 
+    }
+    else
+    {
+      BMIstore= "Error. Range cannot be determined"
+    }
+  
+    results.innerHTML = "Your BMI is " + BMI + message;
+ 
+}, false);
