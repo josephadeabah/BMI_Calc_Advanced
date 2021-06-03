939799f24ba49c048476a6dcec8d539006552e7e
josephadeabah <joseph.adeabah@amalitech.org>
2021-06-03 15:26:11 +0000

    modified my JS and html files and added styling

--- jscript.js
+++ jscript.js
@@ -1,33 +1,5 @@
 
 
-  calculateBMI = (weight, height) => {
-  let BMIstore
-
-  let BMI = Math.round(weight / (height) ** 2)
-  
-  if (BMI < 18.5)
-  {
-     BMIstore = "Your BMI is " + BMI +  ", so you are underweight"
-  }
-  else if (BMI >= 18.5 && BMI < 24.9)
-  {
-    BMIstore = "Your BMI is " + BMI +", so you have a normal weight" 
-  }
-  else if (BMI >= 24.9)
-  {
-    BMIstore = "Your BMI is " + BMI +", so you are overweight."  
-  }
-  else
-  {
-    BMIstore= "Error. Range cannot be determined"
-  }
-
-  return BMIstore
-}
-
-
-/*document.write(calculateBMI(1534,6))*/
-
 
 let first = document.getElementById("btn");
 first.addEventListener('click', () => {
@@ -54,6 +26,6 @@ first.addEventListener('click', () => {
       BMIstore= "Error. Range cannot be determined"
     }
   
-    results.innerHTML = "Your BMI is " + BMI + message;
+    results.innerHTML = "Your BMI is " + BMI +" "+ message;
  
 }, false);
